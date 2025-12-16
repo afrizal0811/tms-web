2bf62684455bab5a7d4fab4ad178a758d1aed241
afrizal0811 <afrizalmaulana0811@gmail.com>
2025-12-16 09:19:01 +0700

    add admin role

--- src/app/page.js
+++ src/app/page.js
@@ -249,7 +249,7 @@ export default function Home() {
           </h2>
           <UserSelectionGrid
             hubId={selectedLocation}
-            roleIds={[ROLE_ID.planner, ROLE_ID.adminPlanner, ROLE_ID.plannerJkt]}
+            roleIds={[ROLE_ID.planner, ROLE_ID.adminPlanner, ROLE_ID.plannerJkt, ROLE_ID.admin]}
             onUserSelect={handleUserSelect}
           />
           <button
