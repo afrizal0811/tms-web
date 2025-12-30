72f43769029a8ccaeb05b4c24bf0a52bff3e638f
afrizal0811 <afrizalmaulana0811@gmail.com>
2025-12-30 15:37:59 +0700

    delete meta data

--- src/app/update-longlat/page.js
+++ src/app/update-longlat/page.js
@@ -1,9 +1,6 @@
 import AppLayout from '@/components/AppLayout';
 import UpdateLonglatPage from '@/features/updateLonglat/UpdateLonglatPage';
 
-export const metadata = {
-  title: 'Update Longlat - TMS Data Processing',
-};
 
 export default function Page() {
   return (
