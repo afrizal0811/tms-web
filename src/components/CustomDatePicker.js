0384753f6fa66410c6a502acaf3f7208df40580f
afrizal0811 <afrizalmaulana0811@gmail.com>
2026-01-05 14:50:25 +0700

    change language in date picker

--- src/components/CustomDatePicker.js
+++ src/components/CustomDatePicker.js
@@ -1,6 +1,13 @@
+// File: src/components/CustomDatePicker.js
 'use client';
 
-import DatePicker from 'react-datepicker';
+import DatePicker, { registerLocale } from 'react-datepicker';
+import { useLanguage } from '@/context/LanguageContext'; 
+import id from 'date-fns/locale/id';
+import enUS from 'date-fns/locale/en-US'; 
+
+registerLocale('id', id);
+registerLocale('en', enUS);
 
 export default function CustomDatePicker({
   className = '',
@@ -17,12 +24,9 @@ export default function CustomDatePicker({
   ...props
 }) {
   const isDisabled = isLoading || disabled;
-
-  // Style dasar yang selalu dipakai
+  const { lang } = useLanguage();
   const baseClasses =
     'px-4 py-2.5 h-[42px] rounded-lg border text-center font-medium shadow-sm transition-colors outline-none w-full';
-
-  // Style berdasarkan state (disabled/loading vs aktif)
   const stateClasses = isDisabled
     ? 'bg-gray-100 text-gray-400 cursor-not-allowed border-gray-200'
     : 'bg-white text-slate-700 cursor-pointer border-gray-300 hover:bg-gray-50';
@@ -46,6 +50,7 @@ export default function CustomDatePicker({
 
   return (
     <DatePicker
+      locale={lang === 'id' ? 'id' : 'en'}
       className={`${baseClasses} ${stateClasses} ${className}`}
       dateFormat={dateFormat}
       dayClassName={handleDayClassName}
