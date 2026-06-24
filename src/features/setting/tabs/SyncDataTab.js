8de0394a537e94a7146b9460c4bc460311d0d9c8
afrizal0811 <afrizalmaulana0811@gmail.com>
2026-06-24 10:22:31 +0700

    add hub name in sync driver data

--- src/features/setting/tabs/SyncDataTab.js
+++ src/features/setting/tabs/SyncDataTab.js
@@ -8,7 +8,7 @@ import Card from '../components/Card';
 
 export default function SyncDataTab({ lastUpdated, onRefresh, isReadOnly, translate }) {
   const [syncLoading, setSyncLoading] = useState({});
-  const { storedLocation: activeHubId } = getLocalStorage();
+  const { storedLocation: activeHubId, storedLocationName } = getLocalStorage();
 
   const executeSync = async (type) => {
     if (isReadOnly) return;
@@ -39,7 +39,7 @@ export default function SyncDataTab({ lastUpdated, onRefresh, isReadOnly, transl
     },
     {
       type: `drivers`,
-      label: translate('setting.tab.sync_data.driver_vehicles'),
+      label: `${translate('setting.tab.sync_data.driver_vehicles')} (${storedLocationName})`,
     },
   ];
 
