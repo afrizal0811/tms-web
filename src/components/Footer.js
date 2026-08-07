570c1887686ec3f4f3d64be48fba62ba0a4d99ed
afrizal0811 <afrizalmaulana0811@gmail.com>
2026-08-07 09:28:44 +0700

    add version in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,26 @@
 'use client';
 
 import { useLanguage } from '@/context/LanguageContext';
+import { getLocalStorage } from '@/lib/localStorageHandler';
+import { useEffect, useState } from 'react';
 
 export default function Footer() {
   const { t } = useLanguage();
   const startYear = 2025;
   const endYear = new Date().getFullYear();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setMounted(true), 0);
+    return () => clearTimeout(timer);
+  }, []);
+
+  const { appVersion } = getLocalStorage();
 
   return (
     <footer className="w-full text-center text-slate-500 dark:text-slate-400 text-xs mt-auto py-2 transition-colors">
+      {mounted && appVersion && ` v${appVersion}`}
+      <br />
       <span suppressHydrationWarning>{t('common.created_by')}</span>: Afi - EDP © {startYear} -{' '}
       {endYear}
     </footer>
